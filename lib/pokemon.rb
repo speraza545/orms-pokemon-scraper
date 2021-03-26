156ab4a660ef17f5a0fcf073e98b6814ab4b8a25
speraza545 <s.peraza545@gmail.com>
2021-03-26 12:33:34 -0400

    commit tree 5e548dbd871ce3b06957c38cbc46e151a30c9553

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,39 @@
+require "pry"
+
 class Pokemon
+    attr_accessor :name, :type, :db
+
+    attr_reader :id
+
+    def initialize(data)
+        @id = data[:id]
+        @name = data[:name]
+        @type = data[:type]
+        @db = data[:db]
+        puts data[:id]
+    end
+
+    def self.save(name, type, db)
+        sql = <<-SQL
+            INSERT INTO pokemon(name, type)
+            VALUES (?, ?)
+        SQL
+        db.execute(sql, name, type)
+    end
+
+    def self.find(id_given, db)
+        puts id_given
+        sql = <<-SQL
+            SELECT *
+            FROM pokemon
+            WHERE pokemon.id = ?
+            LIMIT 1
+        SQL
+        db.execute(sql, id_given).map do |data|
+            passable_data = {:name => data[1], 
+                            :type => data[2], 
+                            :id => data[0]}
+            new_pokemon = self.new(passable_data)
+        end.first
+    end
 end
